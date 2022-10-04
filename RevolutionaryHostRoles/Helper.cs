f42724d3f6ab8dcb2650ffa513d44998e79be108
しゅれ <112616965+oshurecat@users.noreply.github.com>
2022-10-04 23:53:00 +0900

    ハロンのいれた

---
+++ RevolutionaryHostRoles/Helper.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using UnhollowerBaseLib;
+using UnityEngine;
+
+namespace RevolutionaryHostRoles
+{
+    public static class Helpers
+    {
+        private delegate bool DelegateLoadImage(IntPtr tex, IntPtr data, bool markNonReadable);
+        private static DelegateLoadImage _callLoadImage;
+        private static bool LoadImage(Texture2D tex, byte[] data, bool markNonReadable)
+        {
+            _callLoadImage ??= IL2CPP.ResolveICall<DelegateLoadImage>("UnityEngine.ImageConversion::LoadImage");
+            var il2cppArray = (Il2CppStructArray<byte>)data;
+
+            return _callLoadImage.Invoke(tex.Pointer, il2cppArray.Pointer, markNonReadable);
+        }
+        public static unsafe Texture2D loadTextureFromResources(string path)
+        {
+            try
+            {
+                Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, true);
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Stream stream = assembly.GetManifestResourceStream(path);
+                var byteTexture = new byte[stream.Length];
+                var read = stream.Read(byteTexture, 0, (int)stream.Length);
+                LoadImage(texture, byteTexture, false);
+                return texture;
+            }
+            catch
+            {
+                System.Console.WriteLine("Error loading texture from resources: " + path);
+            }
+            return null;
+        }
+        public static Sprite LoadSpriteFromResources(string name, float size)
+        {
+
+            try
+            {
+                var texture = Helpers.loadTextureFromResources(name);
+                return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), size);
+            }
+            catch
+            {
+
+            }
+            return null;
+        }
+        public static bool IsCrew(this PlayerControl p)
+        {
+            return !p.Data.Role.IsImpostor;
+        }
+        public static bool IsImpostor(this PlayerControl p)
+        {
+            return p.Data.Role.IsImpostor;
+        }
+
+    }
+}
---
+++ RevolutionaryHostRoles/Patches/TitleState/TItleButtons.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using Epic.OnlineServices.TitleStorage;
+using HarmonyLib;
+using UnityEngine;
+using static UnityEngine.UI.Button;
+using Object = UnityEngine.Object;
+
+namespace RevolutionaryHostRoles.Patches
+{
+    /* [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
+    */
+    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+    public static class TitleButtons
+    {
+        public static void Postfix()
+        {
+            GameObject button = GameObject.Find("HowToPlayButton").transform.FindChild("Text_TMP").gameObject;
+            GameObject.Destroy(button.GetComponent<TextTranslatorTMP>());
+            TMPro.TextMeshPro text = button.GetComponent<TMPro.TextMeshPro>();
+            text.text = "遊ぶ方";
+            GameObject.Destroy(button.GetComponent<TextTranslatorTMP>());
+        }
+        [HarmonyPatch(typeof(HowToPlayController), nameof(HowToPlayController.Start))]
+        public static class HowToButton
+        {
+            public static bool Prefix()
+            {
+                return true;
+            }
+        }
+    }
+}
