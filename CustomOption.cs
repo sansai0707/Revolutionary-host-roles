29212562a299614251d3cba021e64cff8e55a102
シャンパン2号 <113867056+Shanpan2@users.noreply.github.com>
2022-10-13 16:06:34 +0900

    Update CustomOption.cs
    Configurationのuが抜けてたため修正

--- CustomOption.cs
+++ CustomOption.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.BepInEx.Configration;
+using System.BepInEx.Configuration;
 using UnityEngine;
 
 namespace Revolutionaryhostroles
